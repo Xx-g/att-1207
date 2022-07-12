b8df7998ed7f102d78861e2cad022123631c57d5
Xx-g <guilherme_m_silva4@estudante.sc.senai.br>
2022-07-12 11:20:37 -0300

    First Commit

---
+++ app/src/main/java/com/example/nox/FilmesSeriesAnimes.java
@@ -0,0 +1,14 @@
+package com.example.nox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilmesSeriesAnimes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filmes_series_animes);
+    }
+}
---
+++ app/src/main/java/com/example/nox/LivrosHqsMangas.java
@@ -0,0 +1,14 @@
+package com.example.nox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LivrosHqsMangas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livros_hqs_mangas);
+    }
+}
